83871bc6a90815ca6b5ffe74b3cd642a24439b63
Yhwach <davidmiitarashi@gmail.com>
2015-10-02 17:46:58 -0300

    ngMessages added

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ionic', 'app.controllers'])
+angular.module('app', ['ionic', 'app.controllers', 'ngMessages'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
